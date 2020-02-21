4c6a9128544baef14bf6e98b01c12add6a3e771d
Tobias Oetiker <tobi@oetiker.ch>
2020-02-21 15:34:40 +0100

    support for translation arguments

--- lib/CallBackery.pm
+++ lib/CallBackery.pm
@@ -34,8 +34,7 @@ use CallBackery::Plugin::Doc;
 use CallBackery::Database;
 use CallBackery::User;
 
-our $VERSION = '0.28.0';
-
+our $VERSION = '0.29.0
 use Mojo::Base 'Mojolicious';
 
 =head2 config
