a5520a652877eccfd90f0ce9c47878e74325fc08
Tobias Oetiker <tobi@oetiker.ch>
2020-08-06 14:53:58 +0200

    release 0.34.4

--- lib/CallBackery.pm
+++ lib/CallBackery.pm
@@ -34,7 +34,7 @@ use CallBackery::Plugin::Doc;
 use CallBackery::Database;
 use CallBackery::User;
 
-our $VERSION = '0.34.3';
+our $VERSION = '0.34.4';
 use Mojo::Base 'Mojolicious';
 
 =head2 config
