1b00ff6596631ef49a6190c6c71239d4a516abd9
Tobias Oetiker <tobi@oetiker.ch>
2020-08-07 16:05:51 +0200

    fix === null comparison

--- lib/CallBackery.pm
+++ lib/CallBackery.pm
@@ -34,7 +34,7 @@ use CallBackery::Plugin::Doc;
 use CallBackery::Database;
 use CallBackery::User;
 
-our $VERSION = '0.34.4';
+our $VERSION = '0.34.5';
 use Mojo::Base 'Mojolicious';
 
 =head2 config
