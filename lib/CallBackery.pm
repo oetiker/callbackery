c41b30197595efd91a0325cc1245b2f9b07de6c3
Tobias Oetiker <tobi@oetiker.ch>
2021-08-04 17:18:37 +0200

    fix regression

--- lib/CallBackery.pm
+++ lib/CallBackery.pm
@@ -34,7 +34,7 @@ use CallBackery::Plugin::Doc;
 use CallBackery::Database;
 use CallBackery::User;
 
-our $VERSION = '0.38.13';
+our $VERSION = '0.38.14';
 use Mojo::Base 'Mojolicious';
 
 =head2 config
