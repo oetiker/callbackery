6e90407b0c297b28a37d1b5e11409330bec86fb7
Tobias Oetiker <tobi@oetiker.ch>
2025-09-16 11:26:23 +0200

    0.56.3 2025-09-16 11:25:45 +0200 Tobias Oetiker <tobi@oetiker.ch>
     - activate signatures in abstractaction
    
    0.56.2 2025-09-16 11:16:51 +0200 Tobias Oetiker <tobi@oetiker.ch>
    
     - move all user magic to AbstractAction.pm
     - undefine user after Massage Config
    
    0.56.1 2025-09-15 23:41:28 +0200 Tobias Oetiker <tobi@oetiker.ch>
    
     - set args/formData in the validation phase too
     - fix formData to report data even when there is no selection
    
    0.56.0 2025-09-05 15:52:10 +0200 Tobias Oetiker <tobi@oetiker.ch>
    
     - the GuiPlugin user property is now always defined
       and provides a may method which allows everyone
       access. This makes sure that during startup
       the config system can get all plugins from actionCfg
       lists. You do not have to take special care anymore
       return [] unless $self->user->may('admin');
       is fine now!

--- lib/CallBackery/GuiPlugin/AbstractAction.pm
+++ lib/CallBackery/GuiPlugin/AbstractAction.pm
@@ -20,7 +20,7 @@ The base class for gui forms with actions.
 
 =cut
 
-use Mojo::Base 'CallBackery::GuiPlugin::Abstract';
+use Mojo::Base 'CallBackery::GuiPlugin::Abstract', -signatures;
 
 =head1 ATTRIBUTES
 
@@ -42,6 +42,19 @@ has screenCfg => sub {
         options => $self->screenOpts,
         action  => $self->actionCfg,
     }
+};
+
+# if no user is assigned, all access checks are positive, this allows to explore the
+# modules full capabilities during startup
+has 'user'=> sub ($self) {
+    my $mockUser = bless {}, 'MockUser';
+    unless (defined &MockUser::may) {
+        *MockUser::may = sub ($mockSelf, $right) {
+            # $self->log->debug("MockUser::may($right) -> true");
+            return 1;
+        };
+    }
+    return $mockUser;
 };
 
 =head2 screenOpts
@@ -130,6 +143,10 @@ sub massageConfig {
             $popup->app($self->app);
             $popup->massageConfig($cfg);
         }
+    }
+    if (ref $self->user eq 'MockUser') {
+        # $self->log->debug("massageConfig is done. undefining user");
+        $self->user(undef);
     }
     return;
 }
